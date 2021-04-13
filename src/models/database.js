d5e86483c798486b04fe7dc68beda44104207f18
RachMor <raquel.costt@gmail.com>
2021-04-13 18:46:46 -0300

    conexão com o banco

--- src/models/database.js
+++ src/models/database.js
@@ -1,1 +1,12 @@
-//fazer conexão com o banco de dados
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('testelogin', 'teste', 'testeconf115', {dialect: 'mysql', host: 'localhost'});
+
+try {
+    await sequelize.authenticate();
+    console.log('Conexão estabelecida.');
+  } catch (error) {
+    console.error('Falha na conexão: ', error);
+  }
+
+module.exports = sequelize;
