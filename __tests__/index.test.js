6660ab618fd440a6e8f9d01c2e5e791a8505df56
Thekid666 <jv_100@outlook.com>
2021-04-19 17:48:41 -0300

    teste database

--- __tests__/index.test.js
+++
@@ -1,6 +0,0 @@
-//index.test.js
-const index = require('./user.js')
-test('Validate User', () => {
-    const result = index.validateLogin('firstname', 'lastname', 'email');
-    expect(result).toEqual('John', 'Doe',  'johndoe@email.com');
-})
