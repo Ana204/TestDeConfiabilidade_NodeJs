9f013541a7919ce44ba77b1230212619103a0a7b
ana204 <anagomes.lucia@hotmail.com>
2021-04-19 22:20:32 -0300

    deve criptografar a senha do usuário, test unitario

---
+++ __tests__/unit/user.test.js
@@ -0,0 +1,22 @@
+const bcrypt = require("bcryptjs");
+
+const {users} = require("../../models/");
+const truncate = require("../utils/truncate");
+
+
+describe("User", () => {
+    beforeEach(async () => {
+        await truncate();
+    });
+
+    it("deve criptografar a senha do usuário", async () => {
+        const user = await users.create({
+            email: "Diego@gmail.com.br",
+            password: "123456"
+        })
+
+        const compareHash = await bcrypt.compare("123456", user.password_hash);
+
+        expect(compareHash).toBe(true);
+    })
+})
--- models/User.js
+++ models/User.js
@@ -1,10 +1,22 @@
-const { sequelize } = require(".");
+const bcrypt = require("bcryptjs");
 
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define("users", {
         email: DataTypes.STRING,
+        password: DataTypes.VIRTUAL,
         password_hash: DataTypes.STRING
-    })
+    },
+    //toda vez antes de salvar um novo usuario ele vai setar o campo password_hash
+    {
+        hooks:{
+            beforeSave: async users => {
+               if(users.password){
+                   users.password_hash = await bcrypt.hash(users.password, 8)
+               }
+            }
+        }
+    }
+    )
 
     return User;
 }
